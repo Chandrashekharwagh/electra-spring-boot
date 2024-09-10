2153548c8125b01625bfe5e5ecb5f34ee0837cb0
Chandrashekharwagh <waghchandu2002@gmail.com>
2024-09-10 17:54:53 +0530

    commits

--- src/main/java/com/electra/controller/OrderController.java
+++ src/main/java/com/electra/controller/OrderController.java
@@ -1,4 +1,40 @@
 package com.electra.controller;
 
+import com.electra.domain.Order;
+import com.electra.service.OrderService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
 public class OrderController {
+    @Autowired
+    private OrderService orderService;
+
+    //@RequestMapping(path = "/order", method = RequestMethod.GET)
+    @GetMapping("/order")
+    public List<Order> retrieveCustomers() {
+        return orderService.retrieveOrders();
+    }
+
+    @GetMapping("/order/{orderId}")
+    public String retrieveOrderForOrderId(@PathVariable int orderId) {
+        return orderService.search(orderId);
+    }
+
+    @DeleteMapping("/order/{orderId}")
+    public String deleteOrderForOrderId(@PathVariable int orderId) {
+        return orderService.deleteOrder(orderId);
+    }
+
+    @PostMapping("/order")
+    public String addOrder(@RequestBody Order order) {
+        return orderService.storeOrder(order);
+    }
+
+    @PutMapping("/order/{orderId}")
+    public String updateOrderForOrderId(@PathVariable int orderId, @RequestBody Order order) {
+        return orderService.updateOrder(orderId, order);
+    }
 }
