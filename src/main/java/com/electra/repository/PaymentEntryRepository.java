5277a4e151cb8939454f809169f055a2c4326060
Chandrashekharwagh <waghchandu2002@gmail.com>
2024-09-11 19:09:13 +0530

    commits

--- src/main/java/com/electra/repository/PaymentEntryRepository.java
+++ src/main/java/com/electra/repository/PaymentEntryRepository.java
@@ -1,4 +1,15 @@
 package com.electra.repository;
 
-public class PaymentEntryRepository {
+import java.util.List;
+
+public interface PaymentEntryRepository <Payment>{
+    public String store(Payment payment);
+
+    public List<Payment> retrieve();
+
+    public String search(int id);
+
+    public String delete(int id);
+
+    public String update(Payment payment);
 }
