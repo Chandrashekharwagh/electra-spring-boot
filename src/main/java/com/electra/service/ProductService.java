432814d300e0149cff2d0893b246143336f67235
Chandrashekharwagh <waghchandu2002@gmail.com>
2024-09-14 10:49:13 +0530

    commits

--- src/main/java/com/electra/service/ProductService.java
+++ src/main/java/com/electra/service/ProductService.java
@@ -1,14 +1,11 @@
 package com.electra.service;
 
-import com.electra.domain.Payment;
 import com.electra.domain.Product;
-import com.electra.repository.PaymentRepository;
 import com.electra.repository.ProductRepository;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-
 import java.util.List;
 
 @Service
