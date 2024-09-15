b30122c6ed11fac808fa3ce7d8742a07468e446d
Chandrashekharwagh <waghchandu2002@gmail.com>
2024-09-15 10:10:31 +0530

    commits

--- src/main/java/com/electra/domain/Address.java
+++ src/main/java/com/electra/domain/Address.java
@@ -2,6 +2,9 @@ package com.electra.domain;
 
 import lombok.*;
 
+import java.util.Objects;
+
+
 @Getter
 @Setter
 @Builder
@@ -9,12 +12,34 @@ import lombok.*;
 @AllArgsConstructor
 @ToString
 
+
 public class Address {
-    private int id;
+    private long id;
     private String street;
     private String city;
     private String state;
     private String country;
     private long postalCode;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Address address = (Address) o;
+        return id == address.id &&
+                Objects.equals(street, address.street) &&
+                Objects.equals(city, address.city) &&
+                Objects.equals(state, address.state) &&
+                Objects.equals(country, address.country) &&
+                Objects.equals(postalCode, address.postalCode);
+    }
+
+    // Override hashCode() method
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, street, city, state, country, postalCode);
+    }
 }
+
+
 
