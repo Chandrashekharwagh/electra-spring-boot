d5cbcfe221e3c1f8d68192e97d1a78864ffa2645
Chandrashekharwagh <waghchandu2002@gmail.com>
2024-09-15 11:41:09 +0530

    commits

--- src/main/java/com/electra/controller/CustomerController.java
+++ src/main/java/com/electra/controller/CustomerController.java
@@ -1,5 +1,6 @@
 package com.electra.controller;
 
+import com.electra.domain.Address;
 import com.electra.domain.Customer;
 import com.electra.service.CustomerService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -17,23 +18,28 @@ public class CustomerController {
         return customerService.retrieveCustomers();
     }
 
+
     @GetMapping("/customer/{customerId}")
     public String retrieveCustomerForCustomerId(@PathVariable int customerId) {
         return customerService.search(customerId);
     }
+
+
 
     @DeleteMapping("/customer/{customerId}")
     public String deleteCustomerForCustomerId(@PathVariable int customerId) {
         return customerService.deleteCustomer(customerId);
     }
 
+
     @PostMapping("/customer")
     public String addCustomer(@RequestBody Customer customer) {
         return customerService.storeCustomer(customer);
     }
 
+
     @PutMapping("/customer/{customerId}")
-    public String updateCustomerForCustomerId(@PathVariable int customerId, @RequestBody Customer customer) {
-        return customerService.updateCustomer(customerId, customer);
+    public String updateCustomerForCustomerId(@PathVariable int customerId, @RequestBody Customer customer , Address address) {
+        return customerService.updateCustomer(customerId, customer ,address);
     }
 }
